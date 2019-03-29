1b8dab47f8146481e6bc24145721624f9a30ed37
LC95 <mygamesmylives@gmail.com>
2019-03-29 10:39:12 +0800

    完善命令模式

--- DesignPatternDemo/DP/Commands/Command.cs
+++ DesignPatternDemo/DP/Commands/Command.cs
@@ -1,6 +1,64 @@
+using System;
+using System.Threading;
+
 namespace DP.Commands
 {
-    public class Command
+    public abstract class Command
     {
+        public abstract void Execute();
+    }
+
+    public abstract class Receiver
+    {
+        public abstract void Do();
+    }
+
+    public class Invoker
+    {
+        private Command _command;
+
+        public void SetCommand(Command command)
+        {
+            _command = command;
+        }
+
+        public void SendCommand()
+        {
+            _command.Execute();
+        }
+    }
+
+    public class ConcreteCommand : Command //could be many
+    {
+        private readonly Receiver _receiver;
+
+        public ConcreteCommand(Receiver receiver)
+        {
+            _receiver = receiver;
+        }
+
+        public override void Execute()
+        {
+            _receiver.Do();
+        }
+    }
+
+    public class ConcreteReceiver : Receiver //could be many
+    {
+        public override void Do()
+        {
+            //DO
+        }
+    }
+
+    public class Example
+    {
+        public Example()
+        {
+            var invoker = new Invoker();
+            var command = new ConcreteCommand(new ConcreteReceiver());
+            invoker.SetCommand(command);
+            invoker.SendCommand();
+        }
     }
 }
